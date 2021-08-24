02a6ed3d742c4d393e043f1cefa2f3cf452745fa
kksys <78661652+kksys@users.noreply.github.com>
2021-08-24 22:19:30 +0900

    :sparkles: Added "Check for updates" item on the menu (#2)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 // Flutter imports:
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 // Package imports:
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -13,9 +14,16 @@ import 'package:window_size/window_size.dart';
 
 // Project imports:
 import 'package:spoon_cast_converter/components/pages/home-page.dart';
+import 'package:spoon_cast_converter/models/redux/action/app-actions.dart';
 import 'package:spoon_cast_converter/models/redux/app-state.dart';
 import 'package:spoon_cast_converter/models/redux/middleware/app-middleware.dart';
 import 'package:spoon_cast_converter/models/redux/reducer/app-reducer.dart';
+
+void showCheckForUpdatesDialog({
+  required Store<AppState> store,
+}) {
+  store.dispatch(CheckExistAvailableUpdateAction(launchTime: false));
+}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,33 +31,51 @@ void main() {
     setWindowMinSize(const Size(700, 500));
     setWindowMaxSize(Size.infinite);
   }
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   final store = new Store<AppState>(
     appReducer,
     middleware: rootMiddleware(),
     initialState: const AppState(),
   );
 
+  runApp(MyApp(store: store));
+}
+
+class MyApp extends StatefulWidget {
+  final Store<AppState> store;
+
+  MyApp({
+    Key? key,
+    required this.store,
+  }) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static const menuChannel = const MethodChannel('net.kk_systems.spoonCastConverter.menu');
+
   @override
   void initState() {
     super.initState();
+    menuChannel.setMethodCallHandler(myUtilsHandler);
+  }
+
+  Future<dynamic> myUtilsHandler(MethodCall methodCall) async {
+    switch (methodCall.method) {
+      case 'showCheckForUpdatesDialog':
+        showCheckForUpdatesDialog(store: this.widget.store);
+        break;
+      default:
+        break;
+    }
   }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new StoreProvider(
-      store: store,
+      store: this.widget.store,
       child: MacosApp(
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,23 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+  var controller : FlutterViewController!;
+  var MENU_CHANNEL : FlutterMethodChannel!;
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
+  }
+
+  @IBAction func showCheckForUpdatesDialog(_ sender: NSMenuItem){
+    MENU_CHANNEL.invokeMethod("showCheckForUpdatesDialog", arguments: nil)
+  }
+  
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    controller = mainFlutterWindow.contentViewController as? FlutterViewController
+    MENU_CHANNEL = FlutterMethodChannel(
+      name: "net.kk_systems.spoonCastConverter.menu",
+      binaryMessenger: controller.engine.binaryMessenger
+    )
+    return super.applicationDidFinishLaunching(notification)
   }
 }

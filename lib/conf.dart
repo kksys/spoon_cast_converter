ce5a6363458931d84e7598f851921ca779ad34ab
kksys <kk.systems.develop@gmail.com>
2021-09-22 03:04:13 +0900

    :wrench: Modified the default value for UI_TEST_MODE to false to merge into master

--- lib/conf.dart
+++ lib/conf.dart
@@ -3,4 +3,4 @@ const LATEST_VERSION_API_URI =
     'https://api.github.com/repos/kksys/spoon_cast_converter/releases/latest';
 const USE_DUMMY_AVAILABLE_VERSION_RESPONSE = false;
 const SHOULD_CHECK_UPDATE_AT_LAUNCH = true;
-const UI_TEST_MODE = true;
+const UI_TEST_MODE = false;
